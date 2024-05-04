1b9989746e38e72aed305e0546f67cf83379ceb6
Kai Tung Yu <sam@bristolgram.uk>
2024-05-04 22:09:44 +0100

    add Mecanum wheels

---
+++ controllers/lgdxrobot2/build/release/lgdxrobot2.d
@@ -0,0 +1,5 @@
+build/release/lgdxrobot2.o: lgdxrobot2.cpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Motor.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Device.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/../../c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Robot.hpp
